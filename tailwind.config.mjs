7d8714c51c7836f0cdc95d582276801e84143799
Théo Letouzé <theo.letouze44@gmail.com>
2025-12-24 12:27:22 +0100

    feat: toutes les pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: ['pt-32', 'pt-36', 'pt-40', 'pb-20'],
   theme: {
     extend: {
       colors: {
